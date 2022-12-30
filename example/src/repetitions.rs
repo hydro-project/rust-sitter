9cd592d4131fc412c6dbda11896ec517fb447c6d
Shadaj Laddad <shadaj@users.noreply.github.com>
2022-12-29 17:34:03 -0800

    Make span optional since we can't get it on empty vecs

--- example/src/repetitions.rs
+++ example/src/repetitions.rs
@@ -28,6 +28,31 @@ pub mod grammar {
     }
 }
 
+#[rust_sitter::grammar("repetitions_without_delim")]
+pub mod grammar2 {
+    use rust_sitter::Spanned;
+
+    #[rust_sitter::language]
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    pub struct NumberList {
+        numbers: Spanned<Vec<Spanned<Number>>>,
+    }
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    pub struct Number {
+        #[rust_sitter::leaf(pattern = r"\d+", transform = |v| v.parse().unwrap())]
+        v: i32,
+    }
+
+    #[rust_sitter::extra]
+    struct Whitespace {
+        #[rust_sitter::leaf(pattern = r"\s")]
+        _whitespace: (),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,5 +62,12 @@ mod tests {
         insta::assert_debug_snapshot!(grammar::parse(""));
         insta::assert_debug_snapshot!(grammar::parse("1"));
         insta::assert_debug_snapshot!(grammar::parse("1, 2"));
+    }
+
+    #[test]
+    fn repetitions_grammar2() {
+        insta::assert_debug_snapshot!(grammar2::parse(""));
+        insta::assert_debug_snapshot!(grammar2::parse("1"));
+        insta::assert_debug_snapshot!(grammar2::parse("1 2"));
     }
 }
