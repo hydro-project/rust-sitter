a0487cfb9c118da361464fb7e9c268f532d3f1be
Shadaj Laddad <shadaj@users.noreply.github.com>
2023-03-26 22:22:16 -0700

    Fix WASM sysroot breaking runtime behavior (#37)
    * Fix WASM sysroot breaking runtime behavior
    
    * Add WASM tests to CI

--- example/src/arithmetic.rs
+++ example/src/arithmetic.rs
@@ -1,7 +1,7 @@
 #[rust_sitter::grammar("arithmetic")]
 pub mod grammar {
     #[rust_sitter::language]
-    #[derive(Debug)]
+    #[derive(PartialEq, Eq, Debug)]
     pub enum Expression {
         Number(#[rust_sitter::leaf(pattern = r"\d+", transform = |v| v.parse().unwrap())] i32),
         #[rust_sitter::prec_left(1)]
@@ -28,18 +28,77 @@ pub mod grammar {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use grammar::Expression;
+
+    #[wasm_bindgen_test::wasm_bindgen_test]
+    #[test]
+    fn successful_parses() {
+        assert_eq!(grammar::parse("1").unwrap(), Expression::Number(1));
+
+        assert_eq!(
+            grammar::parse("1 - 2").unwrap(),
+            Expression::Sub(
+                Box::new(Expression::Number(1)),
+                (),
+                Box::new(Expression::Number(2))
+            )
+        );
+
+        assert_eq!(
+            grammar::parse("1 - 2 - 3").unwrap(),
+            Expression::Sub(
+                Box::new(Expression::Sub(
+                    Box::new(Expression::Number(1)),
+                    (),
+                    Box::new(Expression::Number(2))
+                )),
+                (),
+                Box::new(Expression::Number(3))
+            )
+        );
+
+        assert_eq!(
+            grammar::parse("1 - 2 * 3").unwrap(),
+            Expression::Sub(
+                Box::new(Expression::Number(1)),
+                (),
+                Box::new(Expression::Mul(
+                    Box::new(Expression::Number(2)),
+                    (),
+                    Box::new(Expression::Number(3))
+                ))
+            )
+        );
+
+        assert_eq!(
+            grammar::parse("1 * 2 * 3").unwrap(),
+            Expression::Mul(
+                Box::new(Expression::Mul(
+                    Box::new(Expression::Number(1)),
+                    (),
+                    Box::new(Expression::Number(2))
+                )),
+                (),
+                Box::new(Expression::Number(3))
+            )
+        );
+
+        assert_eq!(
+            grammar::parse("1 * 2 - 3").unwrap(),
+            Expression::Sub(
+                Box::new(Expression::Mul(
+                    Box::new(Expression::Number(1)),
+                    (),
+                    Box::new(Expression::Number(2))
+                )),
+                (),
+                Box::new(Expression::Number(3))
+            )
+        );
+    }
 
     #[test]
-    fn arithmetic_grammar() {
-        // successful parses
-        insta::assert_debug_snapshot!(grammar::parse("1"));
-        insta::assert_debug_snapshot!(grammar::parse("1 - 2"));
-        insta::assert_debug_snapshot!(grammar::parse("1 - 2 - 3"));
-        insta::assert_debug_snapshot!(grammar::parse("1 - 2 * 3"));
-        insta::assert_debug_snapshot!(grammar::parse("1 * 2 * 3"));
-        insta::assert_debug_snapshot!(grammar::parse("1 * 2 - 3"));
-
-        // failed parses
+    fn failed_parses() {
         insta::assert_debug_snapshot!(grammar::parse("1 + 2"));
         insta::assert_debug_snapshot!(grammar::parse("1 - 2 -"));
         insta::assert_debug_snapshot!(grammar::parse("a1"));
--- tool/src/wasm-sysroot/stdbool.h
+++ tool/src/wasm-sysroot/stdbool.h
@@ -1,3 +1,3 @@
+#define bool _Bool
 #define true 1
 #define false 0
-#define bool int
