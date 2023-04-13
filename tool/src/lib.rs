88624e302eb82bbcd50844c5dc1be466ea194fcb
Alex Rice <alexrice999@hotmail.co.uk>
2023-04-13 08:49:45 +0000

    Fix typo in warning suppression

--- tool/src/lib.rs
+++ tool/src/lib.rs
@@ -94,7 +94,7 @@ pub fn build_parsers(root_file: &Path) {
         cc::Build::new()
             .include(&dir)
             .include(&sysroot_dir)
-            .flag_if_supported("-Wno-everthing")
+            .flag_if_supported("-Wno-everything")
             .file(dir.path().join("parser.c"))
             .compile(&grammar_name);
     });
